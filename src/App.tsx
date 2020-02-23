d22477b36a6045f2f49d0b7879a07215d0ca0f02
swfz <sawafuji.09@gmail.com>
2020-02-24 04:22:38 +0900

    feat: start,endの日付を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, {ChangeEvent, useEffect, useRef, useState} from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { hot } from 'react-hot-loader';
 
 type InputParams = {
   key: string;
   ids: string[];
+  startDate: string;
+  endDate: string;
 };
 
 type IdsInput = {
@@ -15,34 +17,36 @@ const App: React.FC = () => {
   const initialParams = {
     key: '',
     ids: [],
+    startDate: '',
+    endDate: ''
   };
   const [params, setParams] = useState<InputParams>(initialParams);
+  const [requestParams, setRequestParams] = useState<InputParams>(
+    initialParams
+  );
   const [idsInput, setIdsInput] = useState<IdsInput[]>([
     { index: 0, value: '' },
   ]);
-
-  const apiKeyElem = useRef<HTMLInputElement>(null);
 
   const searchButtonClick = () => {
     console.log('click--------------------');
     console.log(idsInput);
     console.log('click--------------------');
-    const ids = idsInput.map(kv => kv.value);
-    if (apiKeyElem.current) {
-      setParams({
-        key: apiKeyElem.current.value,
-        ids: ids,
-      });
-    }
+    setRequestParams(params);
   };
 
   useEffect(() => {
-    const startDate = '2020-02-17';
-    const endDate = '2020-02-17';
-    const url = `https://toggl.com/reports/api/v2/details.json?workspace_id=${params.ids[0]}&since=${startDate}&until=${endDate}&user_agent=client`;
-    const authString = btoa(`${params.key}:api_token`);
-    console.log(params);
-    console.log(authString);
+    if (!requestParams.key) {
+      return;
+    }
+
+    const startDate = requestParams.startDate;
+    const endDate = requestParams.endDate;
+    const url = `https://toggl.com/reports/api/v2/details.json?workspace_id=${requestParams.ids[0]}&since=${startDate}&until=${endDate}&user_agent=client`;
+    const authString = btoa(`${requestParams.key}:api_token`);
+    console.log('params-----------------');
+    console.log(requestParams);
+    console.log('params-----------------');
     const header = new Headers();
     header.set('Authorization', `Basic ${authString}`);
     const init = {
@@ -56,30 +60,49 @@ const App: React.FC = () => {
       .then(json => {
         console.log(json);
       });
-  }, [params]);
+  }, [requestParams]);
 
   const addIdInput = () => {
     setIdsInput(cur => cur.concat([{ index: cur.length, value: '' }]));
   };
 
+  const keyChangeFn = (e: ChangeEvent<HTMLInputElement>) => {
+    setParams({ ...params, key: e.target.value });
+  };
+  const startChangeFn = (e: ChangeEvent<HTMLInputElement>) => {
+    setParams({ ...params, startDate: e.target.value });
+  };
+  const endChangeFn = (e: ChangeEvent<HTMLInputElement>) => {
+    setParams({ ...params, endDate: e.target.value });
+  };
+
   const partialChangeIdInput = (index: number) => {
     console.log('load partialChageIdInput');
     return (e: ChangeEvent<HTMLInputElement>) => {
-      idsInput[index].value = e.target.value;
+      const id = e.target.value;
+      idsInput[index].value = id;
       setIdsInput(idsInput);
+      const ids = idsInput.map(kv => kv.value);
+      setParams({ ...params, ids: ids });
     };
   };
 
   return (
     <div>
       <div>API Key</div>
-      <input ref={apiKeyElem} type="text" />
+      <input onChange={keyChangeFn} type="text" />
+
+      <div>Start Date</div>
+      <input onChange={startChangeFn} type="date" />
+
+      <div>End Date</div>
+      <input onChange={endChangeFn} type="date" />
 
       <div>Workspace IDs</div>
       <button onClick={addIdInput}>+</button>
       <br />
       {idsInput.map(kv => {
-        return <IdInput index={kv.index} onChange={partialChangeIdInput(kv.index)}/>;
+        return <IdInput key={kv.index} index={kv.index} onChange={partialChangeIdInput(kv.index)}/>;
       })}
 
       <hr />
