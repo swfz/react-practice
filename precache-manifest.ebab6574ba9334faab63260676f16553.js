be58b2f9479cb82022d1efcd3996d27f6668efda
swfz <swfz@users.noreply.github.com>
2020-02-27 13:36:13 +0000

    deploy: 5959db24f9e6e0a2ff3cb74637e000e91390eaa1

---
+++ precache-manifest.ebab6574ba9334faab63260676f16553.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd5adb2add78cdf86b7ee52bd647e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8158c5243d938376a9e9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bb3d3ed9b6648dac35d0",
+    "url": "/static/js/2.dc1a6ff7.chunk.js"
+  },
+  {
+    "revision": "8158c5243d938376a9e9",
+    "url": "/static/js/main.d4ab0017.chunk.js"
+  },
+  {
+    "revision": "648d069155fac54b4183",
+    "url": "/static/js/runtime-main.9c2e5441.js"
+  }
+]);
